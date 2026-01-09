74330135a28b0c90dac5e8eb6887cc81458fe092
Michael Rockson <michaelrockszn@gmail.com>
2026-01-09 16:39:42 +0000

    initial commit

--- Frontend-Task-Manager/src/app/app.ts
+++ Frontend-Task-Manager/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from './component/sidebar-component/sidebar-component';
+import {HomeComponent} from './component/home-component/home-component';
+import {TaskcardComponent} from './component/taskcard-component/taskcard-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, HomeComponent, TaskcardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Frontend-Task-Manager/src/app/component/taskcard-component/taskcard-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taskcard-component',
+  imports: [],
+  templateUrl: './taskcard-component.html',
+  styleUrl: './taskcard-component.css',
+})
+export class TaskcardComponent {
+
+}
