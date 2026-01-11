0d8cc49c6a870a7505a00e2b58c7ad60be38428f
Michael Rockson <michaelrockszn@gmail.com>
2026-01-11 09:16:03 +0000

    feat(home): add dynamic greeting based on current hour

--- Frontend-Task-Manager/src/app/component/home-component/home-component.ts
+++ Frontend-Task-Manager/src/app/component/home-component/home-component.ts
@@ -6,9 +6,37 @@ import { Component } from '@angular/core';
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
-export class HomeComponent {
 
-  userName: string = 'Rockson'
-  subtitle: string = 'Here\'s whats on your plate today'
+export class HomeComponent {
+  currentDate: Date;
+  currentHour: number;
+  userName: string;
+  greeting: string;
+  subtitle: string;
+
+  constructor() {
+    this.currentDate = new Date();
+    this.currentHour = this.currentDate.getHours();
+    this.userName = "Rockson"
+    this.greeting = '';
+    this.subtitle = ''
+    this.setGreeting()
+  }
+
+  setGreeting() {
+    if (this.currentHour >= 0 && this.currentHour <= 11) {
+      this.greeting = "Good Morning";
+      this.subtitle = "Here's whats on your plate this morning";
+
+    } else if (this.currentHour >= 12 && this.currentHour <= 16) {
+      this.greeting = "Good Afternoon";
+      this.subtitle = "Here's whats on your plate this afternoon";
+
+    } else {
+      this.greeting = "Good Evening";
+      this.subtitle = "Here's whats on your plate this evening";
+    }
+  }
+
 
 }
