7b224046b691611c377d89ce37cd306f7397082b
Michael Rockson <michaelrockszn@gmail.com>
2026-01-13 10:58:24 +0000

    feat(API): add flask cors support

--- Backend-Task-Manager/run.py
+++ Backend-Task-Manager/run.py
@@ -3,11 +3,11 @@ from flask_cors import CORS
 from api.task_routes import task
 
 def create_app():
-    application = Flask(__name__)
-    CORS(application)
-    application .register_blueprint(task, url_prefix="/api/tasks")
-    return application
+    task_app = Flask(__name__)
+    CORS(task_app)
+    task_app.register_blueprint(task, url_prefix="/api/tasks")
+    return task_app
 
 if __name__ == "__main__":
-    application  = create_app()
-    application .run(debug=True, host="127.0.0.1", port=5000)
+    task_app = create_app()
+    task_app.run(debug=True, host="127.0.0.1", port=5000)
