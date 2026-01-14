080989784973de3a28bfa1281cc078e48ced1010
Michael Rockson <michaelrockszn@gmail.com>
2026-01-14 20:59:24 +0000

    feat(home): fetch tasks from API and render task cards

--- Frontend-Task-Manager/src/app/app.ts
+++ Frontend-Task-Manager/src/app/app.ts
@@ -1,15 +1,40 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, OnInit } from '@angular/core';
 import {SidebarComponent} from './component/sidebar-component/sidebar-component';
 import {HomeComponent} from './component/home-component/home-component';
 import {TaskcardComponent} from './component/taskcard-component/taskcard-component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, HomeComponent, TaskcardComponent],
+  standalone: true,
+  imports: [
+    SidebarComponent,
+    HomeComponent,
+    TaskcardComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Frontend-Task-Manager');
+  tasks: any[];
+  private apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.tasks = [];
+    this.apiUrl = 'http://localhost:5000/api/tasks/';
+  }
+
+  ngOnInit() {
+    this.getAllTasks();
+  }
+
+  getAllTasks():void {
+    this.http.get<any[]>(this.apiUrl + 'get/tasks/all').subscribe(data => {
+      this.tasks = data;
+      console.log('Tasks loaded:', this.tasks)
+    });
+  }
+
+
 }
